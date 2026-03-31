af9ae04f416cb7058f77c4d727c319048e1dc651
otteremma <Emmanguyen611@gmail.com>
2026-03-31 00:18:02 -0400

    projects

--- App.js
+++ App.js
@@ -140,7 +140,6 @@ export default function App() {
             data={tasks}
             renderItem={renderItem}
             keyExtractor={(item) => item.key}
-            extraData={tasks}
           />
         </View>
             
